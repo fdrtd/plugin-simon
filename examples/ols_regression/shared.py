065f580e399a4b5671f7a064a5b7c448c95cd535
Ballhausen <ballhausen@bytesforlife.com>
2022-02-19 15:35:48 +0100

    move examples to docs repository

--- examples/ols_regression/shared.py
+++
@@ -1,5 +0,0 @@
-URL_ALICE = "http://127.0.0.1:55501"
-URL_BOB = "http://127.0.0.1:55502"
-
-NETWORK = {'nodes': [URL_ALICE, URL_BOB]}
-TOKENS = ["Some", "Shared", "Tokens"]  # should be unique, for match making and synchronization
